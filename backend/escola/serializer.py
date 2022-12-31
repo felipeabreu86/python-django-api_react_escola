cbe7264303a1f7d47dbde93c6436db5a1e54d754
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-12-31 13:54:28 -0300

    adicionando o campo foto

--- backend/escola/serializer.py
+++ backend/escola/serializer.py
@@ -5,13 +5,13 @@ from escola.models import Aluno, Curso, Matricula
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
-        fields = ["id", "nome", "rg", "cpf", "data_nascimento"]
+        fields = ["id", "nome", "rg", "cpf", "data_nascimento", "foto"]
 
 
 class AlunoSerializerV2(serializers.ModelSerializer):
     class Meta:
         model = Aluno
-        fields = ["id", "nome", "celular", "rg", "cpf", "data_nascimento"]
+        fields = ["id", "nome", "celular", "rg", "cpf", "data_nascimento", "foto"]
 
 
 class CursoSerializer(serializers.ModelSerializer):
